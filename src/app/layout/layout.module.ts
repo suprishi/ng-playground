e88fbbe6d530b6a6e4ea7bf15ceadcbe138bde04
Saptarshi <saptarshi.bose@capgemini.com>
2024-03-05 20:48:08 +0530

    Generated menu

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-  declarations: [LayoutComponent],
+  declarations: [LayoutComponent, MenuComponent],
   imports: [CommonModule, SharedModule, RouterModule],
   exports: [LayoutComponent],
 })
