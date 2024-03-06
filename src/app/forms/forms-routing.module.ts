bc3d53fd6c821917d7df6076e1d6a31186ddc8c7
Saptarshi <saptarshi.bose@capgemini.com>
2024-03-06 20:31:16 +0530

    Added nested routes for forms and observables

--- src/app/forms/forms-routing.module.ts
+++ src/app/forms/forms-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsComponent } from './forms.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
+import { ReactiveComponent } from './reactive/reactive.component';
 
-const routes: Routes = [{ path: '', component: FormsComponent }];
+const routes: Routes = [
+  { path: '', component: FormsComponent },
+  { path: 'template-driven-forms', component: TemplateDrivenComponent },
+  { path: 'reactive-forms', component: ReactiveComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class FormsRoutingModule { }
+export class FormsRoutingModule {}
--- src/app/observables/observables-routing.module.ts
+++ src/app/observables/observables-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ObservablesComponent } from './observables.component';
+import { SwitchMapComponent } from './switch-map/switch-map.component';
+import { MergeMapComponent } from './merge-map/merge-map.component';
 
-const routes: Routes = [{ path: '', component: ObservablesComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: ObservablesComponent,
+  },
+  {
+    path: 'switch-map',
+    component: SwitchMapComponent,
+  },
+  {
+    path: 'merge-map',
+    component: MergeMapComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ObservablesRoutingModule { }
+export class ObservablesRoutingModule {}
