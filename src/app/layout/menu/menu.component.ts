35668ce14ed33ad551b03dcd9129286caeb4f4b3
Saptarshi <saptarshi.bose@capgemini.com>
2024-03-11 17:58:16 +0530

    Created the sidenav menu using a tree structure

--- src/app/layout/menu/menu.component.ts
+++ src/app/layout/menu/menu.component.ts
@@ -1,10 +1,81 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-item.model';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import {
+  MatTreeFlatDataSource,
+  MatTreeFlattener,
+} from '@angular/material/tree';
+
+/** Flat node with expandable and level information */
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  route: string;
+  level: number;
+}
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
 })
 export class MenuComponent {
+  menu: MenuItem[] = [
+    {
+      name: 'Observables',
+      route: 'observables',
+      children: [
+        {
+          name: 'SwitchMap',
+          route: 'observables/switch-map',
+        },
+        {
+          name: 'MergeMap',
+          route: 'observables/merge-map',
+        },
+      ],
+    },
+    {
+      name: 'Forms',
+      route: 'forms',
+      children: [
+        {
+          name: 'Template Driven Forms',
+          route: 'forms/template-driven-forms',
+        },
+        {
+          name: 'Reactive Forms',
+          route: 'forms/reactive-forms',
+        },
+      ],
+    },
+  ];
+  private _transformer = (node: MenuItem, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      route: node.route,
+      level: level,
+    };
+  };
 
+  treeControl = new FlatTreeControl<ExampleFlatNode>(
+    (node) => node.level,
+    (node) => node.expandable
+  );
+
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    (node) => node.level,
+    (node) => node.expandable,
+    (node) => node.children
+  );
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+  constructor() {
+    this.dataSource.data = this.menu;
+  }
+
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTreeModule } from '@angular/material/tree';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatSidenavModule, MatButtonModule],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatTreeModule,
+  ],
 })
 export class SharedModule {}
